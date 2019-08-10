5894e4cade2d54cf3daa8c0b4c90e2cd3e50b743
ashil <ashilr11@gmail.com>
2019-08-11 00:17:57 +0200

    data stored for graph, need to get matlabplot working

--- pneumovis/records.py
+++ pneumovis/records.py
@@ -2,7 +2,7 @@
 # There is one record for each particpant
 # The records shows a merge of all incidents regarding the participant
 
-class records:
+class Records:
     def __init__(participantNo, npa_a4_growth, dateCollected, presence, dateOfBirth, sex, hivExposed, site, serotype, sequenceType, vaccineStatus):
         self.participantNo = particpantNo
         self.npa_a4_growth = npa_a4_growth
--- pneumovis/views.py
+++ pneumovis/views.py
@@ -4,10 +4,113 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import Incident
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
 from . import models
 
 def home(request):
+	theIncidents = Incident.objects.all()
+	yaxisNames = ('35B(361)', '21(10854)', '15B/C(8687)', '19A(2062)', '16F(3450)')
+	xaxisNames = ('male', 'hivExposed', 'guguletho', 'mandalay', 'total')
+	list = []
+	maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount = 0
+	for i in theIncidents:
+		if (i.sequence == '361' and i.sex == 'Male'):
+			maleCount += 1
+		if (i.sequence == '361' and i.hivExposed == 'Yes'):
+			hivExposedCount += 1
+		if (i.sequence == '361' and i.site == 'Guguletho'):
+			gugulethoCount += 1
+		if (i.sequence == '361' and i.site == 'Mandalay'):
+			mandalayCount += 1
+		if (i.sequence == '361'.translate(None, "*~")):
+			total += 1
+	list.append([maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount])
+
+	for i in theIncidents:
+		if (i.sequence == '10854' and i.sex == 'Male'):
+			maleCount += 1
+		if (i.sequence == '10854' and i.hivExposed == 'Yes'):
+			hivExposedCount += 1
+		if (i.sequence == '10854' and i.site == 'Guguletho'):
+			gugulethoCount += 1
+		if (i.sequence == '10854' and i.site == 'Mandalay'):
+			mandalayCount += 1
+		if (i.sequence == '10854'.translate(None, "*~")):
+			total += 1
+	list.append([maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount])
+
+	for i in theIncidents:
+		if (i.sequence == '8687' and i.sex == 'Male'):
+			maleCount += 1
+		if (i.sequence == '8687' and i.hivExposed == 'Yes'):
+			hivExposedCount += 1
+		if (i.sequence == '8687' and i.site == 'Guguletho'):
+			gugulethoCount += 1
+		if (i.sequence == '8687' and i.site == 'Mandalay'):
+			mandalayCount += 1
+		if (i.sequence == '8687'.translate(None, "*~")):
+			total += 1
+	list.append([maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount])
+
+	for i in theIncidents:
+		if (i.sequence == '2062' and i.sex == 'Male'):
+			maleCount += 1
+		if (i.sequence == '2062' and i.hivExposed == 'Yes'):
+			hivExposedCount += 1
+		if (i.sequence == '2062' and i.site == 'Guguletho'):
+			gugulethoCount += 1
+		if (i.sequence == '2062' and i.site == 'Mandalay'):
+			mandalayCount += 1
+		if (i.sequence == '2062'.translate(None, "*~")):
+			total += 1
+	list.append([maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount])
+
+	for i in theIncidents:
+		if (i.sequence == '3450' and i.sex == 'Male'):
+			maleCount += 1
+		if (i.sequence == '3450' and i.hivExposed == 'Yes'):
+			hivExposedCount += 1
+		if (i.sequence == '3450' and i.site == 'Guguletho'):
+			gugulethoCount += 1
+		if (i.sequence == '3450' and i.site == 'Mandalay'):
+			mandalayCount += 1
+		if (i.sequence == '3450'.translate(None, "*~")):
+			total += 1
+	list.append([maleCount, hivExposedCount, gugulethoCount, mandalayCount, totalCount])
+
+	data = np.array(list)
+
+	fig = plt.figure()
+	ax = Axes3D(fig)
+
+	lx= len(data[0])            # Work out matrix dimensions
+	ly= len(data[:,0])
+	xpos = np.arange(0,lx,1)    # Set up a mesh of positions
+	ypos = np.arange(0,ly,1)
+	xpos, ypos = np.meshgrid(xpos+0.25, ypos+0.25)
+
+	xpos = xpos.flatten()   # Convert positions to 1D array
+	ypos = ypos.flatten()
+	zpos = np.zeros(lx*ly)
+
+	dx = 0.5 * np.ones_like(zpos)
+	dy = dx.copy()
+	dz = data.flatten()
+
+	ax.bar3d(xpos,ypos,zpos, dx, dy, dz, color='b')
+
+	#sh()
+	ax.w_xaxis.set_ticklabels(xaxisNames)
+	ax.w_yaxis.set_ticklabels(yaxisNames)
+	ax.set_xlabel('Category')
+	ax.set_ylabel('Serotype')
+	ax.set_zlabel('Number of Incidents')
+
+	plt.show()
+
 	return render(request, 'pneumovis/home.html')
 
 def adddata(request):
@@ -28,15 +131,17 @@ def adddata(request):
 		titles = lines[0].split(",")
 
 		if (titles[0] != "participantID" or titles[1] != "npa_a4_growth" or
+			titles[2] != "dateCollected" or titles[3] != "presence" or
 			titles[4] != "dob" or titles[5] != "sex" or
 			titles[6] != "hivExposed" or titles[7] != "site" or
 			titles[8] != "serotype" or titles[9] != "vaccine" or titles[10] != "sequence"):
 			messages.error(request,'CSV file is not in corrrect format, please check the format above and try again')
-			print ("wrong format")
 			return HttpResponseRedirect(reverse("App-adddata"))
+
+		Incident.objects.all().delete()
 		#loop over the lines and save them in db. If error , store as string and then display
-		for line in lines:
-			fields = line.split(",")
+		for i in range(len(lines)-1):
+			fields = lines[i].split(",")
 			participantID = fields[0]
 			npa_a4_growth = fields[1]
 			dateCollected = fields[2]
@@ -51,12 +156,10 @@ def adddata(request):
 			newIncident = Incident(participantID=participantID, npa_a4_growth=npa_a4_growth, dateCollected=dateCollected, presence=presence, dob=dob,sex=sex,
 										hivExposed=hivExposed, site=site, serotype=serotype, vaccine=vaccine, sequence=sequence)
 			newIncident.save()
-			messages.success(request, 'Upload successful')
-			print ("successful")
+		messages.success("Upload Successful")
 
 	except Exception as e:
 		messages.error(request,"Unable to upload file. "+repr(e))
-		print ("unable to upload")
 
 	return HttpResponseRedirect(reverse("App-adddata"))
 
